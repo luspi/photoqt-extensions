05c62b5f7ce4e189c52def5325fbfa9df9fab305
Lukas Spies <Lukas@photoqt.org>
2026-06-23 18:42:33 +0200

    [lang/verification] update localization and verification
    Signed-off-by: Lukas Spies <Lukas@photoqt.org>

--- QuickActions/lang/QuickActions_lt_LT.ts
+++ QuickActions/lang/QuickActions_lt_LT.ts
@@ -8,185 +8,185 @@
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="80"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="273"/>
       <source>Rename file</source>
-      <translation type="unfinished">Rename file</translation>
+      <translation>Pervadinti failą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="54"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="81"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="274"/>
       <source>Copy file</source>
-      <translation type="unfinished">Copy file</translation>
+      <translation>Kopijuoti failą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="55"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="82"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="275"/>
       <source>Move file</source>
-      <translation type="unfinished">Move file</translation>
+      <translation>Perkelti failą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="56"/>
       <source>Delete file (with confirmation)</source>
-      <translation type="unfinished">Delete file (with confirmation)</translation>
+      <translation>Ištrinti failą (patvirtinus)</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="57"/>
       <source>Move file directly to trash</source>
-      <translation type="unfinished">Move file directly to trash</translation>
+      <translation>Perkelti failą į šiukšliadėžę</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="58"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="84"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="277"/>
       <source>Rotate left</source>
-      <translation type="unfinished">Rotate left</translation>
+      <translation>Pasukti kairėn</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="59"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="85"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="278"/>
       <source>Rotate right</source>
-      <translation type="unfinished">Rotate right</translation>
+      <translation>Pasukti dešinėn</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="60"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="86"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="279"/>
       <source>Mirror horizontally</source>
-      <translation type="unfinished">Mirror horizontally</translation>
+      <translation>Apversti veidrodiškai horizontalia kryptimi</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="61"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="87"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="280"/>
       <source>Mirror vertically</source>
-      <translation type="unfinished">Mirror vertically</translation>
+      <translation>Apversti veidrodiškai vertikalia kryptimi</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="62"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="88"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="281"/>
       <source>Crop image</source>
-      <translation type="unfinished">Crop image</translation>
+      <translation>Kirpti vaizdą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="63"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="89"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="282"/>
       <source>Scale image</source>
-      <translation type="unfinished">Scale image</translation>
+      <translation>Keisti vaizdo mastelį</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="64"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="90"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="283"/>
       <source>Tag faces</source>
-      <translation type="unfinished">Tag faces</translation>
+      <translation>Prisegti vardus</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="65"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="91"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="284"/>
       <source>Copy to clipboard</source>
-      <translation type="unfinished">Copy to clipboard</translation>
+      <translation>Kopijuoti į mainų sritį</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="66"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="92"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="285"/>
       <source>Export to different format</source>
-      <translation type="unfinished">Export to different format</translation>
+      <translation>Eksportuoti kitu formatu</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="67"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="93"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="286"/>
       <source>Set as wallpaper</source>
-      <translation type="unfinished">Set as wallpaper</translation>
+      <translation>Nustatyti kaip darbalaukio foną</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="68"/>
       <source>Toggle QR/barcodes</source>
-      <translation type="unfinished">Toggle QR/barcodes</translation>
+      <translation>Perjungti QR/barkodus</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="69"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="95"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="288"/>
       <source>Close window</source>
-      <translation type="unfinished">Close window</translation>
+      <translation>Uždaryti langą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="70"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="96"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="289"/>
       <source>Quit</source>
-      <translation type="unfinished">Quit</translation>
+      <translation>Išeiti</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="204"/>
       <location filename="../QuickActions/qml/QuickActions.qml" line="357"/>
       <source>No file loaded</source>
-      <translation type="unfinished">No file loaded</translation>
+      <translation>Neįkeltas joks failas</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="424"/>
       <source>Reset position to default</source>
-      <translation type="unfinished">Reset position to default</translation>
+      <translation>Perkrauti poziciją į numatytąją</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="432"/>
       <source>Manage in settings manager</source>
-      <translation type="unfinished">Manage in settings manager</translation>
+      <translation>Tvarkyti nustatymų tvarkyklėje</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActions.qml" line="443"/>
       <source>Hide quick actions</source>
-      <translation type="unfinished">Hide quick actions</translation>
+      <translation>Slėpti greituosius veiksmus</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="38"/>
       <source>show quick actions</source>
-      <translation type="unfinished">show quick actions</translation>
+      <translation>rodyti greituosius veiksmus</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="55"/>
       <source>Quick Actions can have any desired order. Some actions refer to other extensions and only function if that extension is installed and enabled. Note that any changes are applied automatically.</source>
-      <translation type="unfinished">Quick Actions can have any desired order. Some actions refer to other extensions and only function if that extension is installed and enabled. Note that any changes are applied automatically.</translation>
+      <translation>Greitieji veiksmai gali būti išdėstyti bet kokią pageidaujamą tvarką. Kai kurie veiksmai susieti su plėtiniais ir veikia tik tuo atveju, jei tas plėtinys yra įdiegtas ir įgalintas. Atminkite, kad visi pakeitimai išsaugomi automatiškai.</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="79"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="290"/>
       <source>separator</source>
-      <translation type="unfinished">separator</translation>
+      <translation>skirtukas</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="83"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="276"/>
       <source>Delete file</source>
-      <translation type="unfinished">Delete file</translation>
+      <translation>Ištrinti failą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="94"/>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="287"/>
       <source>Detect/hide QR/barcodes</source>
-      <translation type="unfinished">Detect/hide QR/barcodes</translation>
+      <translation>Aptikti/paslėpti QR/barkodus</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="154"/>
       <source>Click-and-drag to reorder, right click for more options</source>
-      <translation type="unfinished">Click-and-drag to reorder, right click for more options</translation>
+      <translation>Norint pertvarkyti spustelėti ir vilkti, daugiau parinkčių spustelėjus dešinįjį pelės mygtuką</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="214"/>
       <source>Remove entry</source>
-      <translation type="unfinished">Remove entry</translation>
+      <translation>Pašalinti įrašą</translation>
     </message>
     <message>
       <location filename="../QuickActions/qml/QuickActionsSettings.qml" line="298"/>
       <source>add</source>
       <extracomment>This is written on a button that is used to add a selected block to the status info section.</extracomment>
-      <translation type="unfinished">add</translation>
+      <translation>pridėti</translation>
     </message>
   </context>
 </TS>
